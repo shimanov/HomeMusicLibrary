9f888e44c46bff4f40e543e8cee132a4f0145427
Dmitriy <d.a.shimanov@gmail.com>
2022-08-30 15:59:41 +0700

    2wmez98[closed]

--- HomeMusicLibrary/Entities/Artist.cs
+++ HomeMusicLibrary/Entities/Artist.cs
@@ -1,5 +1,4 @@
 using HomeMusicLibrary.Model;
-using Microsoft.EntityFrameworkCore;
 using Spectre.Console;
 using SpotifyAPI.Web;
 
@@ -9,44 +8,50 @@ public class Artist
 {
     public string token;
     private string artist;
-    private string url;
 
     public async Task ArtistTask()
     {
-        string[] art = File.ReadAllLines(Environment.CurrentDirectory + "/art.txt");
+        string[] art = await File.ReadAllLinesAsync(Environment.CurrentDirectory + "/art.txt");
         if (art.Length != 0)
         {
             foreach (var s in art)
             {
                 artist = s;
                 var spotify = new SpotifyClient(token);
+                int compareResult = 0; 
                 try
                 {
+                    
                     var search = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.Artist, artist));
-                    await foreach (var a in spotify.Paginate(search.Artists, (s) => s.Artists))
+                    await foreach (var a in spotify.Paginate(search.Artists, response => response.Artists))
                     {
                         //TODO: Допилить
-                        var count = -1;
-                        if (s.Take(a.Name.Length).All(e => s[++count] == a.Name[count])) ++count;
-                        AnsiConsole.MarkupLine("[yellow] DEBUG: {0}[/]", count);
-                        
-                        await using var context = new DbContextSqLite();
-                        var entities = new ArtistModel
+                        compareResult = string.Compare(artist, a.Name, StringComparison.OrdinalIgnoreCase);
+                        if (compareResult == 0)
                         {
-                            Id = Guid.NewGuid().ToString(),
-                            ArtistName = a.Name,
-                            ArtistId = a.Id
-                        };
-                        try
-                        {
-                            await context.Artists.AddRangeAsync(entities);
-                            await context.SaveChangesAsync();
-                            AnsiConsole.MarkupLine("[yellow] DEBUG: Artist: {0}\n ID: {1}\n :check_mark_button: Записано в таблицу\n[/]", 
-                                a.Name, a.Id);
+                            await using var context = new DbContextSqLite();
+                            var entities = new ArtistModel
+                            {
+                                Id = Guid.NewGuid().ToString(),
+                                ArtistName = a.Name,
+                                ArtistId = a.Id
+                            };
+                            try
+                            {
+                                await context.Artists.AddRangeAsync(entities);
+                                await context.SaveChangesAsync();
+                                AnsiConsole.MarkupLine(
+                                    "[yellow] DEBUG: Artist: {0}\n ID: {1}\n :check_mark_button: Записано в таблицу\n[/]",
+                                    a.Name, a.Id);
+                            }
+                            catch (Exception e)
+                            {
+                                AnsiConsole.WriteException(e);
+                            }
                         }
-                        catch (Exception e)
+                        else
                         {
-                            AnsiConsole.WriteException(e);
+                            AnsiConsole.MarkupLine("[yellow] DEBUG: {0}[/]", compareResult);
                         }
                     }
                 }
